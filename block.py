8ecce8411fc426ee32886e6b4b6d1bbf637fa4d4
C.V.Hariharan <theroboticlabs@gmail.com>
2018-06-05 14:48:57 +0530

    Primary structure

--- block.py
+++ block.py
@@ -1,15 +1,26 @@
 #10 minutes
-import hashlib
+import hashlib, json
 import transaction
 class Block:
-    def __init__(self, previousHash, transactions):
-        self.previousHash = previousHash
-        self.transactions = transactions
+    def __init__(self, previousHash, transactions, js):
+        if js != None:
+            blocks = json.loads(js)
+            self.previousHash = previousHash
+            self.index = blocks[self.previousHash]['index']+1
+            self.transactions = transactions
+        else:
+            self.previousHash = ""
+            self.index = 0
+            self.transactions = transactions
+        hash_object = hashlib.sha1(str((self.previousHash + str(self.index) + str(self.transactions))).encode())
+        self.hash = hash_object.hexdigest()
     
 
     def getHash(self):
-        hash_object = hashlib.sha1(str((self.previousHash + str(self.transactions))).encode())
-        return hash_object.hexdigest()
+        return self.hash
+
+    def getIndex(self):
+        return self.index
     
     def getAllTransactions(self):
         allTransactions = []
@@ -22,5 +33,21 @@ class Block:
         transactions = self.getAllTransactions()
         block['transactions'] = transactions
         block['previousHash'] = self.previousHash
+        block['index'] = self.index
+        block['hash'] = self.hash
         return block
+
+    def validate(previousBlock, block):
+        if block.previousHash == previousBlock.getHash():
+            if block.getIndex() == (previousBlock.getIndex()+1):
+                return True
+            else:
+                return false
+        else:
+            return false
+
+    def getLongestChain(newBlockchain, Blockchain):
+        if len(newBlockchain) > Blockchain:
+            Blockchain = newBlockchain
+        return Blockchain
 
--- blockchain.py
+++ blockchain.py
@@ -1,34 +1,89 @@
 from block import Block
 from transaction import Transaction
-import json
-from flask import Flask
+import json, random, string, requests
+from flask import Flask, request
 app = Flask(__name__)
 
-# @app.route("/initiate")
-# def genesisBlock():
-#     genesisTransaction = Transaction("","",0);
-#     tl = []
-#     tl.append(genesisTransaction)
-#     genesis = Block("",tl)
-#     with open("blockchain.json", "w") as blockchain:
-#         info = {}
-#         info[genesis.getHash()] = genesis.getAllInfo()
-#         blockchain.write(json.dumps(info))
-#     return "Created genesis block with hash: "+genesis.getHash()
+#Primary node
+pnode = 'http://127.0.0.1:5000/chain'
 
-# tl1 = []
-# tl1.append(Transaction("Adam", "Bob", 2))
-# tl1.append(Transaction("Bob", "Eve", 3))
-# block1 = Block(genesis.getHash(),tl1)
-# print(genesis.getHash())
-# print(block1.getAllInfo())
+Blockchain = {}
+blocksList = []
 
-@app.route('/')
-def createBlock():
-    #Get the active nodes
-    #Create the block
-    #Send a request to the active nodes
+@app.route("/initiate")
+def genesisBlock():
+    genesisTransaction = Transaction("","",0);
+    tl = []
+    tl.append(genesisTransaction)
+    genesis = Block("",tl,None)
+    blocksList.append(genesis)
+    Blockchain[genesis.getHash()] = genesis.getAllInfo()
+    # with open("blockchain.json", "w") as blockchain:
+    #     info = {}
+    #     info[genesis.getIndex()] = genesis.getAllInfo()
+    #     blockchain.write(json.dumps(info))
+    return "Created genesis block with hash: "+genesis.getHash()
+
+@app.route("/chain")
+def printBlockchain():
+    return json.dumps(Blockchain)
+    # info = {}
+    # for block in Blockchain:
+    #     info[block.getIndex()] = block.getAllInfo()
+        
+    # content = ""
+    # with open("blockchain.json", "r") as blockchain:
+    #     for line in blockchain.readlines():
+    #         content = content + line
+    # return content
+
+@app.route("/addblock", methods=['GET', 'POST']) 
+def addBlock():
+    info = ""
+    if request.method == 'POST':  
+       info = request.form.get('blockInfo')
+    blockInfo = json.loads(info)
+    #Get the entire blockchain
+    r = requests.get(pnode)
+    latestBlockchain = json.loads(r.text)
+
+    Blockchain[blockInfo['hash']] = blockInfo
+    
+    #Create t
+
+
+# def generateRandomString(num):
+#     return ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(num)])
+
+# def dummyTransactions(num):
+#     t = []
+#     for x in range(num):
+#         t.append(Transaction(generateRandomString(),generateRandomString(),random.randint(1,100)))
+#     return t
+
+# def getBlockchain():
+#     #Get the updated blockchain from a hard-coded primary node
+
+
+
+# @app.route('/')
+# def createBlock():
+#     #Get the blockchain
+#     getBlockchain()
+#     #Create the block
+ #Parse the transactions
+    # allTransactions = []
+    # snippets = transactions.split(",")
+    # for snippet in snippets:
+    #     source = snippet.split(":")[0]
+    #     dest = snippet.split(":")[1]
+    #     value = int(snippet.split(":")[2])
+    #     allTransactions.append(Transaction(source,dest,value))
+#     #Send a request to the active nodes
 
 if __name__ == '__main__':
+    #Open blockchain.json and add it to a list
+    with open("blockchain.json",'r') as b:
+        json.loads(b.read())
     port = input("Port: ")
     app.run("127.0.0.1",port)
